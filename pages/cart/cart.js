afcbf3d4b158299b3bb787f7d34f88e69173743f
lfj <liao_fengji@163.com>
2019-09-26 18:33:05 +0800

    完成购物车的静态页面

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -5,7 +5,49 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    presentList:[
+      '30天无忧退货',
+      '48小时快速退款',
+      '满88元免邮费'
+    ],
+    cartList: [
+      {
+        "id": 129,
+        "user_id": "oQmbb4sNZdxaUQZ0sfYgvtOP2S7c",
+        "goods_id": 1015007,
+        "goods_name": "典雅美式全棉刺绣抱枕",
+        "retail_price": 59,
+        "number": 3,
+        "list_pic_url": "http://yanxuan.nosdn.127.net/a2045004de8a6225289376ad54317fc8.png"
+      },
+      {
+        "id": 135,
+        "user_id": "oQmbb4sNZdxaUQZ0sfYgvtOP2S7c",
+        "goods_id": 1009024,
+        "goods_name": "日式和风懒人沙发",
+        "retail_price": 599,
+        "number": 2,
+        "list_pic_url": "http://yanxuan.nosdn.127.net/149dfa87a7324e184c5526ead81de9ad.png"
+      },
+      {
+        "id": 130,
+        "user_id": "oQmbb4sNZdxaUQZ0sfYgvtOP2S7c",
+        "goods_id": 1030002,
+        "goods_name": "160*230羊毛圈绒枪刺地毯",
+        "retail_price": 899,
+        "number": 1,
+        "list_pic_url": "http://yanxuan.nosdn.127.net/8b9328496990357033d4259fda250679.png"
+      },
+      {
+        "id": 136,
+        "user_id": "oQmbb4sNZdxaUQZ0sfYgvtOP2S7c",
+        "goods_id": 1035006,
+        "goods_name": "全棉单面割绒浴室地垫",
+        "retail_price": 56,
+        "number": 2,
+        "list_pic_url": "http://yanxuan.nosdn.127.net/ee92704f3b8323905b51fc647823e6e5.png"
+      }
+    ]
   },
 
   /**
